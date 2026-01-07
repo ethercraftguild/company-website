288f6c7c377d3135918f1f3414beab38a89bb1f6
Mohammad Mustakim Hassan <mmhshayer@gmail.com>
2026-01-07 16:26:10 +0600

    chore: Disable Twitter and YouTube social links in config.

--- src/config.ts
+++ src/config.ts
@@ -9,8 +9,8 @@ export const SITE_CONFIG = {
         linkedin: "https://linkedin.com/company/ethercraft-guild",
         github: "https://github.com/ethercraft-guild",
         facebook: "https://facebook.com/ethercraft-guild",
-        twitter: "https://twitter.com/ethercraftguild",
-        youtube: "https://youtube.com/c/ethercraftguild"
+        // twitter: "https://twitter.com/ethercraftguild",
+        // youtube: "https://youtube.com/c/ethercraftguild"
     }
 };
 
